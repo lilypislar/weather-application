247f71e58f2778477f8d1bdd9a210c524a36dc3e
lilypislar <122724157+lilypislar@users.noreply.github.com>
2023-01-29 10:06:42 +0100

    Showing the current temperture

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("Hello");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "06bdaa9ff41fb26956c5ea7fe3ab59fa";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
