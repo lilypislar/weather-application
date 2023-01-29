ef1155138f1470a6d697115fa96151b5d65c61e8
lilypislar <122724157+lilypislar@users.noreply.github.com>
2023-01-29 19:42:54 +0100

    Added the search engine

--- src/index.js
+++ src/index.js
@@ -33,8 +33,17 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "06bdaa9ff41fb26956c5ea7fe3ab59fa";
-let city = "Warsaw";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "06bdaa9ff41fb26956c5ea7fe3ab59fa";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Warsaw");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
